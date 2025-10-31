a9a31cf9f1eadb8af251c6281430b96333b9dcac
aditya kankal <adityabuisness12@gmail.com>
2025-10-31 22:54:01 +0530

    gemini service made

---
+++ backend/src/services/geminiService.js
@@ -0,0 +1,6 @@
+import { GoogleGenerativeAI, GenerationConfig } from '@google/generative-ai';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
